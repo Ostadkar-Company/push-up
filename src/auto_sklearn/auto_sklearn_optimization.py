b31211290ffcc1248dfa8671ebe631cc30e25244
abbasjariani <jariani.abb@gmail.com>
2021-12-21 11:30:03 +0330

    ss

--- src/auto_sklearn/auto_sklearn_optimization.py
+++ src/auto_sklearn/auto_sklearn_optimization.py
@@ -3,8 +3,10 @@ import os
 import shutil
 import sklearn.datasets
 import sklearn.metrics
-
+import PipelineProfiler
 import autosklearn.classification
+import matplotlib.pyplot as plt
+import pickle
 
 
 if __name__ == "__main__":
@@ -26,25 +28,18 @@ if __name__ == "__main__":
 	feat_type = ['Categorical', 'Categorical', 'Numerical', 'Categorical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Categorical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical', 'Numerical']
 	dataset_name = 'ostadkar_cleaning_fulfillment'
 	time_left_for_this_task=60*60*24
-	per_run_time_limit=60*20
+	per_run_time_limit=60*5
 	initial_configurations_via_metalearning = None
-	max_models_on_disc = None
+	max_models_on_disc = 80
 	seed = 1367
-	memory_limit = 14*1000
+	memory_limit = 13*1000
 	resampling_strategy = 'cv'
 	resampling_strategy_arguments = {'folds': 5}
-	tmp_folder='autosklearn'
-	output_folder = 'autosklearn_out'
-	delete_output_folder_after_terminate = True
-	delete_tmp_folder_after_terminate = True
+	tmp_folder='/home/abbas/myProjects/211128_ostadkar_fullfillment/push-up/src/auto_sklearn/out2/tmp2'
+	output_directory = '/home/abbas/myProjects/211128_ostadkar_fullfillment/push-up/src/auto_sklearn/out2'
+	delete_output_folder_after_terminate = False
+	delete_tmp_folder_after_terminate = False
 	n_jobs = -1
-
-	if os.path.exists(os.getcwd() + '/' + output_folder):
-		#os.remove(os.getcwd() + '/' + output_folder)
-		shutil.rmtree(os.getcwd() + '/' + output_folder)
-	if os.path.exists(os.getcwd() + '/' + tmp_folder):
-		shutil.rmtree(os.getcwd() + '/' + tmp_folder)
-
 
 	#help(autosklearn.classification.AutoSklearnClassifier)
 
@@ -58,12 +53,52 @@ if __name__ == "__main__":
 		memory_limit = memory_limit,
 		resampling_strategy = resampling_strategy,
 		resampling_strategy_arguments = resampling_strategy_arguments,
-		tmp_folder = tmp_folder#,
-		#output_folder = output_folder,
-		#e_tmp_folder_after_terminate = delete_tmp_folder_after_terminate,
-		#delete_output_folder_after_terminate = delete_output_folder_after_terminate
+        #output_folder=output_directory,
+		tmp_folder = tmp_folder,
+		#,
+		delete_tmp_folder_after_terminate = delete_tmp_folder_after_terminate,
+		delete_output_folder_after_terminate = delete_output_folder_after_terminate
 
 	)
 	automl.fit(X_train, y_train,
 		feat_type = feat_type,
 		dataset_name = dataset_name)
+	profiler_data = PipelineProfiler.import_autosklearn(automl)
+	PipelineProfiler.plot_pipeline_matrix(profiler_data)
+	#    with open('/home/abbas/disppage.htm','wb') as f:   # Use some reasonable temp name
+	#        f.write(PipelineProfiler.plot_pipeline_matrix(profiler_data).html.encode("UTF-8"))
+	
+	with open('/home/abbas/myProjects/211128_ostadkar_fullfillment/push-up/src/auto_sklearn/automl-classifier.pkl', 'wb') as f:
+		pickle.dump(automl, f)    
+    # load model
+    #with open('iris-classifier.pkl', 'rb') as f:
+    #    loaded_classifier = pickle.load(f)
+
+
+predictions = automl.predict(X_test)
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, predictions))
+
+
+poT = automl.performance_over_time_
+poT.plot(
+    x='Timestamp',
+    kind='line',
+    legend=True,
+    title='Auto-sklearn accuracy over time',
+    grid=True,
+)
+plt.show()
+
+from sklearn.inspection import plot_partial_dependence, permutation_importance
+
+r = permutation_importance(automl, X_test, y_test, n_repeats=10, random_state=0)
+sort_idx = r.importances_mean.argsort()[::-1]
+
+plt.boxplot(r.importances[sort_idx].T,
+            labels=[list(set(list(mydata))-set(['label_wn_quote']))[i] for i in sort_idx])
+
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
+
+plt.savefig('books_read.png')
